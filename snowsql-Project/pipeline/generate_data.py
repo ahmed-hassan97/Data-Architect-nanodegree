bd4bdc7b8cfea25701f8e33088dfaaf2354d49e6
Ahmed Mohamed Gaber <ahmed.gaber@gizasystems.local>
2023-04-14 23:36:24 +0200

    adding python project

--- snowsql-Project/pipeline/generate_data.py
+++ snowsql-Project/pipeline/generate_data.py
@@ -8,12 +8,12 @@ import json
 # set the S3 credentials and bucket information
 aws_access_key_id = 'AKIAW5AFKK2PBX6MCT4M'
 aws_secret_access_key = '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'
-csv_bucket_name = 'material-price-csv'
-json_bucket_name = 'material-price-json'
+stock1_bucket_name = 'material-price-stock1'
+stock2_bucket_name = 'material-price-stock2'
 Material = ["oil","wood","gas"]
 
 
-def generate_material_data(day , month):
+def generate_material_data_stock1(day , month):
     # generate a list of timestamps with minutes
     start_time = datetime(2022,month, int(day+1), 0, 0, 0)
     end_time = datetime(2022, month, int(day+1), 23, 59, 59)
@@ -34,6 +34,25 @@ def generate_material_data(day , month):
 
     return df , date
 
+def generate_material_data_stock2(day , month):
+    # generate a list of timestamps with minutes
+    start_time = datetime(2022,month, int(day+1), 0, 0, 0)
+    end_time = datetime(2022, month, int(day+1), 23, 59, 59)
+    time_diff = timedelta(minutes=1)
+    timestamps = pd.date_range(start=start_time, end=end_time, freq=time_diff)
+
+    # generate a list of numbers
+    min_value = [random.randint(100.0, 120.0) for i in range(len(timestamps))]
+    max_value = [random.randint(110.0, 130.0) for i in range(len(timestamps))]
+    material = [random.choice(Material) for i in range(len(timestamps))]
+    # create a dictionary of data
+    data = {'timestamp': timestamps,'material': material,'min_value': min_value , 'max_value': max_value}
+    date = start_time.date()
+    date = str(date).replace('-','_')
+    # create a DataFrame from the dictionary
+    df = pd.DataFrame(data)
+
+    return df , date
 
 def connect_aws():
 
@@ -43,15 +62,24 @@ def connect_aws():
                     aws_secret_access_key=aws_secret_access_key)
     
     for i in range(30):
-        df ,date = generate_data(day=i , month=3)
+        df_1 ,date = generate_material_data_stock1(day=i , month=3)
+        df_2 ,date = generate_material_data_stock2(day=i , month=3)
         # Convert the DataFrame to a CSV file
-        csv_buffer = StringIO()
+        csv_buffer_stock1 = StringIO()
+        csv_buffer_stock2 = StringIO()
+        df_1.to_csv(csv_buffer_stock1, index=False)
+        df_2.to_csv(csv_buffer_stock2, index=False)
+        
+        csv_buffer_stock1.seek(0)
+        csv_buffer_stock2.seek(0)
+        
+        csv_object_key_stock1 = '{}.csv'.format(date)
+        csv_object_key_stock2 = '{}.csv'.format(date)
+       
+        s3.put_object(Bucket=stock1_bucket_name, Key=csv_object_key_stock1, Body=csv_buffer_stock1.getvalue())
+        s3.put_object(Bucket=stock2_bucket_name, Key=csv_object_key_stock2, Body=csv_buffer_stock2.getvalue())
 
-        df.to_csv(csv_buffer, index=False)
-        data_dict = df.to_dict(orient='index')
-        csv_buffer.seek(0)
-       
-        csv_object_key = '{}.csv'.format(date)
-       
-        s3.put_object(Bucket=csv_bucket_name, Key=csv_object_key, Body=csv_buffer.getvalue())
-      
+
+
+if __name__ == "__main__":
+    connect_aws()
